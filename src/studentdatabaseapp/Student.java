b8e9bbc905bde59b910857509fc04d4699c28fb5
DESKTOP-0PM4NBR\Stefcio <tefcio5@gmail.com>
2018-03-16 18:59:43 +0100

    Initial commit

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,10 @@
+package studentdatabaseapp;
+
+/**
+ * Created by Stefcio on 16.03.2018.
+ */
+public class Student {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,7 @@
+package studentdatabaseapp;
+
+/**
+ * Created by Stefcio on 16.03.2018.
+ */
+public class StudentDatabaseApp {
+}
