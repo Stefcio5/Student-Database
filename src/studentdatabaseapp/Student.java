fa7b36f28799bce1c6af085128b8b05152a1f3ab
DESKTOP-0PM4NBR\Stefcio <tefcio5@gmail.com>
2018-03-16 19:43:17 +0100

    Created constructor, ID generator, enroll in courses

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,10 +1,60 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 /**
  * Created by Stefcio on 16.03.2018.
  */
 public class Student {
-    public static void main(String[] args) {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses = null;
+    private int tuitionBalance;
+    private static int costOfCourse = 600;
+    private static int ID = 1000;
 
+
+    // Constructor: prompt user to enter student's name and year
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("Enter student class level: ");
+        this.firstName = in.nextLine();
+
+        setStudentID();
     }
+    // Generate ID
+    private void setStudentID(){
+        ID++;
+        this.studentID = ID;
+    }
+    // Enroll in courses
+    public void enroll(){
+        do {
+            System.out.println("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")){
+                courses = courses + "\n" + course;
+                tuitionBalance += costOfCourse;
+            }
+            else {
+                break;
+            }
+        }
+        while (1 != 0);
+        System.out.println("Enrolled in: " + courses);
+        System.out.println("Tuition balance: " + tuitionBalance);
+    }
+    // View balance
+
+    // Pay tuition
+
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,4 +4,12 @@ package studentdatabaseapp;
  * Created by Stefcio on 16.03.2018.
  */
 public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        st1.enroll();
+
+        // Ask how many new students to add
+
+        // Create n number of new students
+    }
 }
